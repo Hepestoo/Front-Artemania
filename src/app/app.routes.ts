110d34d8ea4cb7da16819354144b45fa4c130d39
Hepestoo <teckalan2@gmail.com>
2025-04-15 18:14:30 -0500

    login y registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 import { SobreNosotrosComponent } from './pages/sobre-nosotros/sobre-nosotros.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { CarritoComponent } from './pages/carrito/carrito.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegistroComponent } from './pages/auth/registro/registro.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
@@ -12,5 +14,7 @@ export const routes: Routes = [
   { path: 'sobre-nosotros', component: SobreNosotrosComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'carrito', component: CarritoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent},
   { path: '**', redirectTo: '' } // fallback
 ];
